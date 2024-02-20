cd275bb515adb3b67cdc8ad123274cb96582f966
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-02-20 14:25:48 +1000

    page layout

--- src/components/Nbar.jsx
+++ src/components/Nbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Nbar = () => {
   return (
     <header className="header">
-      <NavLink to='/' className='w-10 rounded-lg bg-red items-center justify-center flex font-bold shadow-md'>
+      <NavLink to='/' className='w-14 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
         <p className='blue-gradient_text'>ZMS</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const [form, setForm] = useState({name: '', email: '', message:''})
+
+  const [isLoading, setisLoading] = useState(false)
+
+  const handleChange = () => {}
+  const handleFocus = () => {} //in
+  const handleBlur = () => {} //out
+
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get In Touch</h1>
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='Alex'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus = {handleFocus}
+              onBlur = {handleBlur}
+              />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='alex@email.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus = {handleFocus}
+              onBlur = {handleBlur}
+              />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows={4}
+              className='textarea'
+              placeholder='Let me know how I can help you!'
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus = {handleFocus}
+              onBlur = {handleBlur}
+              />
+          </label>
+          <button
+            type="submit"
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleBlur}
+            onBlur={handleBlur}
+          >
+            Send
+            {isLoading? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
