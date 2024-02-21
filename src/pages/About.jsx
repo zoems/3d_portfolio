ece95bbc5c37e8a053e443ddb3841fe7410e94b8
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-02-21 12:32:32 +1000

    add experience

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import React from 'react'
-import { skills } from '../constants/'
+import { skills, experiences } from '../constants/'
+import CTL from '../components/CTL.jsx';
+
+
 
 const About = () => {
   return (
@@ -45,9 +50,54 @@ const About = () => {
             I've worked in all sorts of places in the desert in Australia to the US and Germany.
             have a look at my
             <a className='blue-gradient_text'
-            href="www.linkedin.com/in/zoe-mawby-smart">linkedIn</a> for the full story. </p>
+            href="www.linkedin.com/in/zoe-mawby-smart"> linkedIn</a> for the full story. </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                icon={
+                  <div className='flex items-center justify-center w-full h-full'>
+                    <img src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                iconStyle={{background: experience.iconBg}}
+                contentStyle={{
+                  borderBottom: '8px',
+                  borderStyle: 'solid',
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: 'none'
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p className='text-black-500 font-medium font-base' style={{margin:0}}>
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li key={`experience-poit-${index}`} className='text-black-500/50 font-normal pl-1 text-sm'>
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
+      <hr className='border-slate-200' />
+      <CTL />
     </section>
   )
 }
