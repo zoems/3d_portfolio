225b5dc7bd8d1940faf37cd23bae90293d378b58
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-02-21 12:49:59 +1000

    bug fixes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -69,8 +69,8 @@ const Welcome = () => {
           />
           <Plane
             isRotating = {isRotating}
-            planeScale = {planeScale}
-            planePosition = {planePosition}
+            scale = {planeScale}
+            position = {planePosition}
             rotation = {[0, 20, 0]}
           />
         </Suspense>
